47097b78164175efbec93ff7a17bb83fde588a99
Mspiro <dhobale53@gmail.com>
2022-03-21 14:47:40 +0530

    Login and register page popup created

--- themes/custom/shopspot/src/js/loginMenu.js
+++ themes/custom/shopspot/src/js/loginMenu.js
@@ -119,16 +119,63 @@
       var loginMenu =
         document.getElementsByClassName("menu--login")[0].children[1]
           .childNodes[1].childNodes[3];
+
       $(document).ready(function () {
         $(loginbtn).hover(
           function () {
             $(loginMenu).css("display", "flex");
           },
           function () {
-            $(loginMenu).css("display", "none");
+            setTimeout(function () {
+              $(loginMenu).css("display", "none");
+            }, 2000);
           }
         );
       });
+    },
+  };
+})(jQuery, Drupal);
+
+(function ($, Drupal) {
+  Drupal.behaviors.loginPageModal = {
+    attach: function (context, setting) {
+      var beforBox = document.querySelector("#drupal-modal");
+      var classname = beforBox.children[0].classList.value;
+      var sidePannelImg = document.createElement('p');
+      sidePannelImg.classList.add('bottom-img');
+      if (classname == "user-login-form") {
+        var login_para = document.createElement("p");
+        login_para.classList.add("login-para");
+        var login_heading = document.createElement("h2");
+        var login_titel = document.createTextNode("Login");
+        var login_subtext = document.createElement("h5");
+        var login_text = document.createTextNode(
+          "Get access to your Orders, Wishlist and Recommendations"
+        );
+        login_subtext.appendChild(login_text);
+        login_heading.appendChild(login_titel);
+        login_para.appendChild(login_heading);
+        login_para.appendChild(login_subtext);
+        login_para.appendChild(sidePannelImg);
+        beforBox.appendChild(login_para);
+      }
+      else{
+        var register_para = document.createElement("p");
+        register_para.classList.add("register-para");
+        var register_heading = document.createElement("h2");
+        var register_titel = document.createTextNode("Looks like you're new here!");
+        var register_subtext = document.createElement("h5");
+        var register_text = document.createTextNode(
+          "Sign up with your mobile number to get started"
+        );
+        
+        register_subtext.appendChild(register_text);
+        register_heading.appendChild(register_titel);
+        register_para.appendChild(register_heading);
+        register_para.appendChild(register_subtext);
+        register_para.appendChild(sidePannelImg);
+        beforBox.appendChild(register_para);
+      }
     },
   };
 })(jQuery, Drupal);
